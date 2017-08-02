7f8222e044b16fc8866e26bcb1b74ab65180fafc
hosysy <hosy@nurigo.net>
2017-08-02 12:36:58 +0900

    server.sh 수정

--- server.sh
+++ server.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-./hugo server -b wiley.dev.nurigo.net --bind 0.0.0.0 --theme=docuapi
-=======
 ./hugo server -b https://wiley.dev.nurigo.net/ --bind 0.0.0.0 --theme=docuapi
->>>>>>> 74750a2b8a3fdf5e9e63431e78770a22e8502dcf
